58255a6e66a35e5165d34f17d73124ada27f9f62
crossy87 <hannabmcross@gmail.com>
2017-11-12 14:19:06 +0000

    first test added and user schema set up begun

--- test/controllers/authentications_test.js
+++ test/controllers/authentications_test.js
@@ -13,7 +13,39 @@ describe('Authentications', function() {
   });
 
   //describe the POST request to the registration api
+  describe('POST /api/register', function(){
   // it syntax with text explaining the task and followed by a function which posts, sets and sends sample log-in data based on the user model.
-  // end syntax which passes 'err' and 'res' as arguments and 'expects' responses as specified by us
-  //complete the test with 'done' and it will move to the next one.
+    it('should register a user with the correct credentials', function  (done) {
+      api
+        .post('/api/register')
+        .set('Accept', 'application/json')
+        .send({
+          user: {
+            userType: 'shop',
+            username: 'sam',
+            email: 'sam@sam.com',
+            password: 'password',
+            passwordConfirmation: 'password'
+          }
+        })
+        // end syntax which passes 'err' and 'res' as arguments and 'expects' responses as specified by us
+        .end((err, res) => {
+          expect(res.status).to.be.eq(200);
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.eq('Thanks for registering');
+          expect(res.body.token).to.be.a('string');
+          //complete the test with 'done' and it will move to the next one.
+          done();
+        });
+    });
+    xit('should not register a user with no email', function() {
+    });
+    xit('should not register a user with no password', function() {
+    });
+    xit('should not register a user with no password confirmation', function() {
+    });
+  });
+
+
+
 });
