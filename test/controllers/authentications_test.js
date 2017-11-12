95595c14b2f130ff1702798afebe7db6447772f1
crossy87 <hannabmcross@gmail.com>
2017-11-12 11:40:01 +0000

    added auth test file in tests folder

--- test/controllers/authentications_test.js
+++ test/controllers/authentications_test.js
@@ -1,1 +1,8 @@
 /* globals api, expect, describe, xdescribe, beforeEach, afterEach, it, xit */
+
+require('../spec_helper');
+// const User = require('../../models/user');
+
+describe('Authentications', function() {
+
+});
