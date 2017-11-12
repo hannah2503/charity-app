c4f672df4362adb46e93f3e965af8026bb1ffc62
crossy87 <hannabmcross@gmail.com>
2017-11-12 11:55:12 +0000

    some psudocode in auth tests and routes files

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+//require controllers and libraries
+// restful routes
 
-//need restful routes
-
+//home route
+//index route
+//new route
+//id show and delete route
+//id edit route
 
 module.exports = router;
--- test/controllers/authentications_test.js
+++ test/controllers/authentications_test.js
@@ -5,4 +5,11 @@ require('../spec_helper');
 
 describe('Authentications', function() {
 //HANNAH ADDED THIS FILE
+
+// drop the database before each instance of the test
+
+//describe the POST request to the registration api
+// it syntax with text explaining the task and followed by a function which posts, sets and sends sample log-in data based on the user model.
+// end syntax which passes 'err' and 'res' as arguments and 'expects' responses as specified by us
+//complete the test with 'done' and it will move to the next one. 
 });
