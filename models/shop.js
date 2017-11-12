862bb36f94ae87fd3ebcb99b17afc842926d9d17
camawjones <camawjones@gmail.com>
2017-11-12 18:55:01 +0000

    shop schema created

---
+++ models/shop.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: { type: String, required: true},
+  createdBy: {type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+});
+
+commentSchema.methods.belongsTo = function commentBelongsTo(user) {
+  if(typeof this.createdBy.id === 'string') return this.createdBy.id === user.id;
+  return user.id === this.createdBy.toString();
+};
+
+const shopSchema = new mongoose.Schema({
+  name: String,
+  address: {
+    line1: { type: String, required: true },
+    line2: String,
+    city: { type: String, required: true },
+    postcode: { type: String, required: true },
+    country: { type: String, required: true }
+  },
+  email: { type: String, required: true, unique: true },
+  number: { type: Number, required: true, unique: true },
+  bio: { type: String, required: true },
+  image: { type: String, required: true },
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  comments: [ commentSchema ]
+});
+
+shopSchema.methods.belongsTo = function shopBelongsTo(user) {
+  if(typeof this.createdBy.id === 'string') return this.createdBy.id === user.id;
+  return user.id === this.createdBy.toString();
+};
+
+module.exports = mongoose.model('Shop', shopSchema);
