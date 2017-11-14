c35c2dc6d68c18d3e05774cdf4462b3cb55f18ea
camawjones <camawjones@gmail.com>
2017-11-14 09:45:00 +0000

    about to figure out gmaps

--- src/js/directives/googleMaps.js
+++ src/js/directives/googleMaps.js
@@ -12,7 +12,6 @@ function googleMap($window) {
     template: '<div class="google-map"></div>',
     link(scope, element) {
       const mapCenter = new $window.google.maps.LatLng(51.5, -0.07);
-      // let text;
 
       const map = new $window.google.maps.Map(element[0], {
         zoom: 15,
@@ -21,26 +20,17 @@ function googleMap($window) {
 
       var infowindow = new $window.google.maps.InfoWindow();
 
-      // const infoWindow = new $window.google.maps.InfoWindow();
-
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
           var pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
           };
-
-          // infoWindow.setPosition(pos);
-          // infoWindow.setContent('Location found.');
-          // infoWindow.open(map);
           map.setCenter(pos);
         }, function() {
           handleLocationError(true, map.getCenter());
-          // handleLocationError(true, infoWindow, map.getCenter());
         });
       } else {
-        // Browser doesn't support Geolocation
-        // handleLocationError(false, infoWindow, map.getCenter());
         handleLocationError(false, map.getCenter());
       }
 
@@ -101,35 +91,6 @@ function googleMap($window) {
         infowindow.open(map, marker);
       }
 
-        // var infowindow = new $window.google.maps.InfoWindow();
-        //
-        // function (place) {
-        // $window.google.maps.event.addListener(marker, 'click', function() {
-        //   infowindow.setContent('<div>'place.formatted_address'</div>');
-        //       infowindow.open(map, this);
-        //     });
-        //   }
-
-
-
-
-
     }
   };
 }
-
-//   const request = {
-//     location: center,
-//     radius: '500',
-//     type: [ 'restaurant']
-//   };
-//   service = new google.maps.places.PlacesService(map);
-//   service.nearbySearch(request, callback);
-// }
-// function callback(results, status) { if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       const place = results[i];
-//       createMarker(results[i])
-//   }
-// }
-// }
