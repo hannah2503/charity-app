e25e9b8c87a86cc1d3b5f370a2403bf7366d292b
odwyercatriona <odwyercatriona@gmail.com>
2017-11-15 19:26:02 +0000

    save deleteuser

--- src/js/controllers/users/users-show.controller.js
+++ src/js/controllers/users/users-show.controller.js
@@ -2,9 +2,20 @@ angular
   .module('charityApp')
   .controller('usersShowController', usersShowController);
 
-usersShowController.$inject = ['$stateParams', 'User'];
-function usersShowController($stateParams, User){
+usersShowController.$inject = ['$stateParams', 'User', '$state'];
+function usersShowController($stateParams, User, $state){
   const vm = this;
+
+  vm.delete = userDelete;
+
+  function userDelete() {
+    User
+      .remove({id: vm.user._id})
+      .$promise
+      .then(()=> {
+        $state.go('home');
+      });
+  }
 
   User
     .get({id: $stateParams.id})
