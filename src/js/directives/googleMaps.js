f57a0db70da410cb6fdbc6e3f351022b21978d0b
crossy87 <hannabmcross@gmail.com>
2017-11-16 14:04:45 +0000

    merging errors

--- src/js/directives/googleMaps.js
+++ src/js/directives/googleMaps.js
@@ -48,9 +48,8 @@ function googleMap($window, $timeout) {
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(
-          browserHasGeolocation
-            ? 'Error: The Geolocation service failed.'
-            : "Error: Your browser doesn't support geolocation."
+          browserHasGeolocation?'Error: The Geolocation service failed.'
+            :'Error: Your browser doesn\'t support geolocation.'
         );
         infoWindow.open(map);
       }
