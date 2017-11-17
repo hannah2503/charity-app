bf2327d96424f1b781623e32c0f0cb91a7fe2f66
camawjones <camawjones@gmail.com>
2017-11-17 12:22:49 +0000

    final commit

--- controllers/users.js
+++ controllers/users.js
@@ -3,19 +3,29 @@ const User = require('../models/user');
 function usersIndex(req, res) {
   User.find()
     .exec()
-    .then((users) =>
-      res.status(200).json(users))
-    .catch(() => res.status(500).json({ message: 'Oh no! Something went wrong. Please try again!' }));
+    .then(users => res.status(200).json(users))
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: 'Oh no! Something went wrong. Please try again!' })
+    );
 }
 
 function usersShow(req, res) {
   User.findById(req.params.id)
     .exec()
     .then(user => {
-      if (!user) return res.status(404).json({ message: 'We couldn\'t find this user. Please try again!' });
+      if (!user)
+        return res
+          .status(404)
+          .json({ message: "We couldn't find this user. Please try again!" });
       return res.status(200).json(user);
     })
-    .catch(() => res.status(500).json({ message: 'Oh no! Something went wrong. Please try again!' }));
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: 'Oh no! Something went wrong. Please try again!' })
+    );
 }
 
 function usersUpdate(req, res) {
@@ -25,19 +35,30 @@ function usersUpdate(req, res) {
   })
     .exec()
     .then(user => {
-      if (!user) return res.status(404).json({ message: 'We couldn\'t find this user. Please try again!' });
+      if (!user)
+        return res
+          .status(404)
+          .json({ message: "We couldn't find this user. Please try again!" });
       return res.status(200).json({ user });
     })
-    .catch(() => res.status(500).json({ message: 'Oh no! Something went wrong. Please try again!' }));
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: 'Oh no! Something went wrong. Please try again!' })
+    );
 }
 
 function usersDelete(req, res) {
   User.findByIdAndRemove(req.params.id)
     .exec()
-    .then((user) => {
-      if (!user) return res.status(404).json({ message: 'We couldn\'t find this user. Please try again!' });
+    .then(() => {
+      return res.status(200).json({ message: 'User successfully deleted!' });
     })
-    .catch(() => res.status(500).json({ message: 'Oh no! Something went wrong. Please try again!' }));
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: 'Oh no! Something went wrong. Please try again!' })
+    );
 }
 
 module.exports = {
